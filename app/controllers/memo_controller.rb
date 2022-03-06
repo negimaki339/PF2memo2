f142b1703b679440efa46f2c2119673c97067967
negimaki339 <negimaki0620@gmail.com>
2022-03-06 13:11:08 +0000

    [indexページ編集]

--- app/controllers/memo_controller.rb
+++ app/controllers/memo_controller.rb
@@ -5,4 +5,14 @@ class MemoController < ApplicationController
 
  def new
  end
+ 
+ def create
+   Post.create(post_params)
+   redirect_to root_path
+ end
+
+private
+def post_params
+　params.permit(:content)
+end
 end
